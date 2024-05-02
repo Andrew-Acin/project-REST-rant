8f3104a42ab2389cb28322dfae4e0e2b3d923e81
Andrew Acin <andrew_acin@hotmail.com>
2024-05-01 21:18:57 -0700

    adding 404 page route

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
@@ -5,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
 
 
