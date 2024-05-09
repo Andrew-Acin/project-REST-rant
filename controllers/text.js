8319d17e01bacdb7e5358430ef2874c35ffd723f
Andrew Acin <andrew_acin@hotmail.com>
2024-05-08 18:32:05 -0700

    fixed controllers up to date now

--- controllers/text.js
+++ controllers/text.js
@@ -1,27 +1,16 @@
-// const express = require('express')
-const router = require('express').Router()
-const places = require('../models/places.js')
-
-// GET /places
 router.get('/', (req, res) => {
     res.render('places/index', { places })
-
 })
 
-
-// GET /places/new
 router.get('/new', (req, res) => {
-    console.log(req.body)
     res.render('places/new')
 })
 
-
-// POST /places
 router.post('/', (req, res) => {
-    // console.log(req.body)
+    console.log(req.body)
     if (!req.body.pic) {
         // Default image if one is not provided
-        req.body.pic = 'public/images/kitten.jpg'
+        req.body.pic = 'http://placekitten.com/400/400'
     }
     if (!req.body.city) {
         req.body.city = 'Anytown'
@@ -30,11 +19,7 @@ router.post('/', (req, res) => {
         req.body.state = 'USA'
     }
     places.push(req.body)
-    console.log(places)
     res.redirect('/places')
 })
 
-
-
-
-module.exports = router;
+module.exports = router
