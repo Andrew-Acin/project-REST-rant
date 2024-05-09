ad4c5719ca5bc9d3ba8caf003e50078fb1d6439e
Andrew Acin <andrew_acin@hotmail.com>
2024-05-08 21:25:28 -0700

    delete functionality implemeted

--- controllers/places.js
+++ controllers/places.js
@@ -48,6 +48,22 @@ router.get('/:id', (req, res) => {
     }
 })
 
+// delete route
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+})
+
+
 
 
 
